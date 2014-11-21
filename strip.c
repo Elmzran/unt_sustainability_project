692ac575d64f8d46e93e42bf73cb075a4c01f5e6
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2014-11-21 15:18:03 -0600

    Add initial variables for strip.c

---
+++ strip.c
@@ -0,0 +1,17 @@
+/*
+Author: Jonathan Roosa (JonathanRoosa@my.unt.edu)
+
+Date: 11/21/14
+
+Purpose: Strip out all tabs and [ Yahoo! Maps ] content from the
+business lists for the data project.
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	FILE	*fp;
+	char	str[1024];
+
+}
