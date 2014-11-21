e07e14fc9cb5516b768b6079e8867d040f957cb3
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2014-11-21 15:30:56 -0600

    Add first draft of strip.c, iterates through the business data, separating each piece of data onto its own line and removing Yahoo! Maps.

--- strip.c
+++ strip.c
@@ -4,14 +4,34 @@ Author: Jonathan Roosa (JonathanRoosa@my.unt.edu)
 Date: 11/21/14
 
 Purpose: Strip out all tabs and [ Yahoo! Maps ] content from the
-business lists for the data project.
+business lists for the data project, forcing each piece of data
+on its own line
 */
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char *argv[])
 {
-	FILE	*fp;
-	char	str[1024];
+	FILE	*fold, *fnew;
+	char	str[512];
 
+	/* Open the file for reading */
+	fold = fopen(argv[1], "r");
+	fnew = fopen(argv[2], "w");
+
+	/* Scan through the file*/
+	while (feof(fold) == 0)
+	{
+		/* Read in the current line */
+		fscanf(fold, "%[^\n\t]s", str);
+		/* Checks if string contains "Yahoo!" */
+		if (strstr(str, "Yahoo!") == NULL)
+			/* If the string doesn't contain "Yahoo!", then print it out in the new file */
+			fprintf(fnew, "%s\n", str);
+	}
+
+	/* Close both files */
+	fclose(fold), fclose(fnew);
+
+	return 0;
 }
