7ce1b7c9ce1aae5ef6dd1ce16daae4e00b40bf38
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2014-11-21 17:23:11 -0600

    Add additional format.c functionality

--- format.c
+++ format.c
@@ -13,27 +13,28 @@ perfect importing into excel.
 
 /* 59 = ; char dec val */
 #define DELIN 59
+#define MAX_SIZE 256
 
 /* Function prototypes for each data set */
-void	find_company(char **str);
-void	find_address(char **str);
-void	find_city_state_zip(char **str);
-void	find_country(char **str);
-void	find_website(char **str);
-void	find_phone(char **str);
-void	find_tax(char **str);
-void	finx_contact(char **str);
-void	find_description(char **str);
-void	find_naics(char **str);
-void	find_sales(char **str);
-void	find_exmployees(char **str);
+void	find_company(FILE *fp, char str[][MAX_SIZE], int num);
+void	find_address(FILE *fp, char str[][MAX_SIZE], int num);
+void	find_city_state_zip(FILE *fp, char *str[], int num);
+void	find_country(FILE *fp, char *str[], int num);
+void	find_website(FILE *fp, char *str[], int num);
+void	find_phone(FILE *fp, char *str[], int num);
+void	find_tax(FILE *fp, char *str[], int num);
+void	finx_contact(FILE *fp, char *str[], int num);
+void	find_description(FILE *fp, char *str[], int num);
+void	find_naics(FILE *fp, char *str[], int num);
+void	find_sales(FILE *fp, char *str[], int num);
+void	find_exmployees(FILE *fp, char *str[], int num);
 
 /* Begin execution */
 int main(int argc, char *argv[])
 {
 	FILE	*fold, *fnew;
-	char	str[16][256];
-	int	i = 0;
+	char	str[16][MAX_SIZE];
+	int	i, j;
 
 	/* Open up the files */
 	fold = fopen(argv[1], "r");
@@ -43,7 +44,56 @@ int main(int argc, char *argv[])
 	while (feof(fold) == 0)
 	{
 		/* Load in all data for one business */
-		for (i = 0; strstr(str, "# OF EMPLOYEES:") == NULL; ++i)
+		for (i = 0; strstr(str[i - 1], "# OF") == NULL; ++i)
+		{
+			/* Scan in the data to the current string */
 			fscanf(fold, "%[^\n]s", str[i]);
+			/* Skip the newline */
+			getc(fold);
+		}
+		
+		/* Iterate through all functions for proper formatting of data,
+		so it can be imported into excel */
+		find_company(fnew, str, i);
+		find_address(fnew, str, i);
+
+		/* Zero out all of the data. Will cause segfault if a for loop is used */
+		str[0][0] = '\0';
+		str[1][0] = '\0';
+		str[2][0] = '\0';
+		str[3][0] = '\0';
+		str[4][0] = '\0';
+		str[5][0] = '\0';
+		str[6][0] = '\0';
+		str[7][0] = '\0';
+		str[8][0] = '\0';
+		str[9][0] = '\0';
+		str[10][0] = '\0';
 	}
+
+	/* Close the files */
+	fclose(fold), fclose(fnew);
+
+	return 0;
+}
+
+/* Search for and print the company name, always first (?) */
+void find_company(FILE *fp, char str[][MAX_SIZE], int num)
+{
+	/* Print out the first data string, since it's always the company name */
+	fprintf(fp, "%s%c\n", str[0], DELIN);
+
+	/* Empty the string */
+	str[0][0] = '\0';	
+
+	return;
+}
+
+/* Search for and print the address, will not contain any of the unique data of the other sets,
+and is never first */
+void find_address(FILE *fp, char str[][MAX_SIZE], int num)
+{
+	i = 0;
+
+	/**/
 }
